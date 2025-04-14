bf2dfba7d163be98be3da484083a58fcaf3f5531
Ruike Lyu (吕睿可) <47710448+Rick10119@users.noreply.github.com>
2025-04-14 09:42:06 -0400

    更新参数读取

--- data_prepare/plot_typical_load.m
+++ data_prepare/plot_typical_load.m
@@ -1,7 +1,29 @@
 %% Illustrative Price and Energy Consumption at Various Stages of a Steel Powder Plant on a Certain Day
 
 % Load parameters
-parameter_Lu_milp;
+% Referencing the paper by Lu-2021-data-driven
+filename = "load_parameters_Lu_milp.xlsx";
+
+% Using data from Lu-2021 for validation
+load_parameter = xlsread(filename);
+
+% Energy consumption coefficients
+e_np =  load_parameter(:, [2, 4, 6]);
+
+% Material production coefficients
+g_np =  load_parameter(:, [1, 3, 5]);
+
+% Material maximum/minimum values (0), raw materials are generally not limited
+S_max = load_parameter(:, 7);
+S_max(end) = 400;
+
+% Initial material values
+S_0 = 0.5 * S_max;
+S_0(end) = 0;
+
+% Material target values (change)
+S_tar = zeros(size(S_max));
+S_tar(end) = 10 * 24; % The bottleneck stage needs to operate for 24 hours.
 
 % Load prices (August)
 data_price_2;
